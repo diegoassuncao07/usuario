27647c69ac4bbc410c808152f124a0273b093cf9
Diego <diego_assuncao07@outlook.com>
2026-05-26 15:56:01 -0300

    adicionado as classes de security

---
+++ src/main/java/com/projeto_Diego/usuario/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,10 @@
+package com.projeto_Diego.usuario.infrastructure.repository;
+
+import com.diego.aprendendospring.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone, Long> {
+
+}
