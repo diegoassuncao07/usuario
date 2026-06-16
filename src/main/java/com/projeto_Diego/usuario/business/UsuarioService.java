a1296ae563f5144a50f8cb2894c585a775592a0a
Diego <diego_assuncao07@outlook.com>
2026-06-16 10:29:48 -0300

    inclusao de metodo put de usuario

--- src/main/java/com/projeto_Diego/usuario/business/UsuarioService.java
+++ src/main/java/com/projeto_Diego/usuario/business/UsuarioService.java
@@ -6,6 +6,7 @@ import com.projeto_Diego.usuario.infrastructure.entity.Usuario;
 import com.projeto_Diego.usuario.infrastructure.exceptions.ConflictException;
 import com.projeto_Diego.usuario.infrastructure.exceptions.ResourceNotFoundException;
 import com.projeto_Diego.usuario.infrastructure.repository.UsuarioRepository;
+import com.projeto_Diego.usuario.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,7 @@ public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
     public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO){
         emailExiste(usuarioDTO.getEmail());
@@ -50,5 +52,15 @@ public class UsuarioService {
         usuarioRepository.deleteByEmail(email);
     }
 
+    public UsuarioDTO atualizarDadosUsuario(String token, UsuarioDTO dto){
+        String email = jwtUtil.extractUsername(token.substring(7));
+        dto.setSenha(dto.getSenha() != null ? passwordEncoder.encode(dto.getSenha()) : null);
 
+        Usuario usuarioEntity = usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new ResourceNotFoundException("Email não localizado"));
+
+        Usuario usuario = usuarioConverter.updateUsuario(dto, usuarioEntity);
+
+        return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
+    }
 }
