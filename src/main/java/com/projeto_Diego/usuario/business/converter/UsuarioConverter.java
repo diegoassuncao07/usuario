c629a6fdc2b286a3fb7ef33038286588efa28c61
Diego <diego_assuncao07@outlook.com>
2026-08-12 10:46:53 -0300

    cria nova exception e classe globalexceptionhandler

--- src/main/java/com/projeto_Diego/usuario/business/converter/UsuarioConverter.java
+++ src/main/java/com/projeto_Diego/usuario/business/converter/UsuarioConverter.java
@@ -19,8 +19,10 @@ public class UsuarioConverter {
                 .nome(usuarioDTO.getNome())
                 .email(usuarioDTO.getEmail())
                 .senha(usuarioDTO.getSenha())
-                .enderecos(paraListaEndereco(usuarioDTO.getEnderecos()))
-                .telefones(paraListaTelefone(usuarioDTO.getTelefones()))
+                .enderecos(usuarioDTO.getEnderecos() != null ?
+                        paraListaEndereco(usuarioDTO.getEnderecos()) : null)
+                .telefones(usuarioDTO.getTelefones() != null ?
+                        paraListaTelefone(usuarioDTO.getTelefones()) : null)
                 .build();
     }
 
